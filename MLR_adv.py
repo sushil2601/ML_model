b37996928ff61d9a9f83ceca8b130c25ed58a043
sushil2601 <sushilsuman2355@gmail.com>
2021-04-03 23:15:27 +0530

    First commit

---
+++ MLR_adv.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/home/sushil/Documents/code_ml/advertising.csv')
+
+df.head()
+
+df.shape
+
+df.columns
+
+df.isnull().sum()
+
+X = df.iloc[:,:-1]
+
+X.head()
+
+y = df.iloc[:,3]
+
+y.head()
+
+plt.hist(df['TV'])
+
+plt.boxplot(df.TV)
+
+plt.hist(df.Radio)
+
+plt.boxplot(df.Radio)
+
+plt.hist(df.Newspaper)
+
+plt.boxplot(df.Newspaper)
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 0)
+
+import statsmodels.formula.api as smf
+
+from sklearn.linear_model import LinearRegression
+
+model = smf.ols('Sales~TV+Radio+Newspaper',data = df).fit()
+
+model.params
+
+model.summary()
+
+### model2
+
+model2 = smf.ols('Sales~TV+Radio',data = df).fit()
+
+model2.params
+
+model2.summary()
+
